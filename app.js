ce849c6348f24deae6d8d9a16de7a4a9d954ef6f
Reuben Goodridge <rggiyfe@protonmail.com>
2023-06-07 20:44:21 +0100

    Added basic structure and function for first panel

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 let position = 1
+let docState = 1
 
 function next() {
   const current = document.getElementById(`panel${position}`);
-  position = position + 1
+  console.log(position += 1)
   const next = document.getElementById(`panel${position}`);
-
   // Move panel1 to the left
   current.style.transform = 'translateX(-100%)';
 
   // Bring panel2 from the right
   next.style.transform = 'translateX(0)';
+  
+  // Trigger timeline animation if on panel2
+  const timeline = document.getElementById('timeline');
+  if(position === 3) {
+    timeline.classList.add('timeline-animation');
+  }
 }
 
 function prev() {
   const current = document.getElementById(`panel${position}`);
-  position = position - 1
+  console.log(position -= 1)
   const prev = document.getElementById(`panel${position}`);
 
   // Move panel1 to the left
@@ -22,4 +28,18 @@ function prev() {
 
   // Bring panel2 from the right
   prev.style.transform = 'translateX(0)';
+}
+
+function portrait() {
+  const doc = document.getElementById('document')
+  const portrait = document.getElementById('portrait')
+  if (docState === 1) {
+    doc.style.transform = 'translateX(-135%)'
+    portrait.style.transform = 'translateY(-0%)'
+    docState = 2
+  } else {
+    doc.style.transform = 'translateX(-50%)'
+    portrait.style.transform = 'translateY(110%)'
+    docState = 1
+  }
 }
